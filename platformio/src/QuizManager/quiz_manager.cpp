de3a014664a087f1f08612373cb5239213365882
Timo Haßler <timo.hassler@vector.com>
2023-07-21 23:12:22 +0200

    * Proper implementation of quiz manager

--- platformio/src/QuizManager/quiz_manager.cpp
+++ platformio/src/QuizManager/quiz_manager.cpp
@@ -4,35 +4,54 @@
 #include <cstdlib>
 #include <musical_notes.h>
 
-int QuizManager::GetNrOfCorrectTones(uint32_t middle_frequency,
-                               uint32_t high_frequency) {
-  Serial.println((String)"Middle: " + middle_frequency + " High: " + high_frequency);
-  // TODO
-  if (middle_frequency < (high_frequency - 50)) {
-    Serial.println("CORRECT!");
-    return 2;
-  } else {
-    Serial.println("Keep trying.");
-    return 0;
-  }
+namespace {
+const std::vector<music::NOTE> POSSIBLE_ROOT_NOTES {
+  music::NOTE::C4, music::NOTE::C4S, music::NOTE::D4, music::NOTE::D4S, music::NOTE::E4, music::NOTE::F4,
+      music::NOTE::F4S, music::NOTE::G4, music::NOTE::G4S, music::NOTE::A4, music::NOTE::A4S, music::NOTE::B4,
+};
+}// namespace
+
+QuizManager::QuizManager() {
+  GenerateNewRootTone();
 }
 
-bool QuizManager::IsEasterEggTriggered(uint32_t middle_frequency,
-                                       uint32_t high_frequency) {
-  // TODO
-  return false;
+int QuizManager::GetNrOfCorrectTones(uint32_t middle_frequency, uint32_t high_frequency) {
+  Serial.println((String) "Root: " + root_tone_ + " Middle: " + middle_frequency + " High: " + high_frequency);
+  auto correct_tones = 0;
+
+  if (IsMiddleNoteCorrect(middle_frequency)) {
+    Serial.println("Middle note is correct!");
+    ++correct_tones;
+  }
+
+  if (IsHighNoteCorrect(high_frequency)) {
+    Serial.println("High note is correct!");
+    ++correct_tones;
+  }
+
+  return correct_tones;
+}
+
+bool QuizManager::IsEasterEggTriggered(uint32_t middle_frequency, uint32_t high_frequency) {
+  return ((middle_frequency > 790) && (high_frequency > 790));
 }
 
 uint32_t QuizManager::GetRootFrequency() {
-  // TODO
-  return music::NOTE::C4;
+  return root_tone_;
 }
 
 void QuizManager::GenerateNewRootTone() {
-  // TODO
-  if (root_tone_ == music::C4) {
-    root_tone_ = music::F4;
-  } else {
-    root_tone_ = music::C4;
-  }
+  auto random_index = std::rand() % POSSIBLE_ROOT_NOTES.size();
+  root_tone_ = POSSIBLE_ROOT_NOTES.at(random_index);
+  Serial.println((String) "New root tone frequency: " + root_tone_);
+}
+
+bool QuizManager::IsMiddleNoteCorrect(uint32_t middle_frequency) {
+  auto expected_frequency = root_tone_ * (5 / 4);
+  return (abs((int) (expected_frequency - middle_frequency)) < FREQUENCY_TOLERANCE_);
+}
+
+bool QuizManager::IsHighNoteCorrect(uint32_t high_frequency) {
+  auto expected_frequency = root_tone_ * (3 / 2);
+  return (abs((int) (expected_frequency - high_frequency)) < FREQUENCY_TOLERANCE_);
 }
--- platformio/src/QuizManager/quiz_manager.h
+++ platformio/src/QuizManager/quiz_manager.h
@@ -2,9 +2,12 @@
 
 #include <cstdint>
 #include <musical_notes.h>
+#include <vector>
 
 class QuizManager {
 public:
+ QuizManager();
+
   int GetNrOfCorrectTones(uint32_t middle_frequency, uint32_t high_frequency);
   bool IsEasterEggTriggered(uint32_t middle_frequency, uint32_t high_frequency);
 
@@ -12,6 +15,9 @@ public:
   void GenerateNewRootTone();
 
  private:
-  music::NOTE root_tone_ = music::C4;  // TODO randomize initial tone
-  // TODO map<Tone, frequency>
+  music::NOTE root_tone_;
+  double FREQUENCY_TOLERANCE_ = 5;
+
+  bool IsMiddleNoteCorrect(uint32_t middle_frequency);
+  bool IsHighNoteCorrect(uint32_t high_frequency);
 };
